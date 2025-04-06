a7f24c0c6584adb2ca03c15db494305773178e01
ROCS17 <rocsdev1704@gmail.com>
2025-04-06 13:53:38 -0500

    feat(codewars): add solution for kata041 to kata045

---
+++ src/codewars/kata043.py
@@ -0,0 +1,20 @@
+"""
+Double Char
+
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+Examples (Input -> Output):
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "
+"""
+
+
+def double_char(s: str):
+    word = ""
+    for i in s:
+        word += i * 2
+    return word
+
+
+print(double_char("Hello"))
---
+++ src/codewars/kata045.py
@@ -0,0 +1,26 @@
+"""
+Unique In Order
+
+Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+For example:
+
+unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+unique_in_order([1, 2, 2, 3, 3])   == [1, 2, 3]
+unique_in_order((1, 2, 2, 3, 3))   == [1, 2, 3]
+
+"""
+
+
+def unique_in_order(sequence):
+    result = []
+    prev = None
+    for item in sequence:
+        if item != prev:
+            result.append(item)
+            prev = item
+    return result
+
+
+print(unique_in_order("AAAABBBCCDAABBB"))
